a5dc47f6f0b6a8179686606b8bd707c0a635909c
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2024-05-01 19:05:19 +0000

    added js and css

--- js/script.js
+++ js/script.js
@@ -1,34 +1,33 @@
+
+
+// Data
 // https://assets.codepen.io/16425/web-3-spring-2024-roster.json
+
 
 // Data source
 const dataSource = 'https://assets.codepen.io/16425/web-3-spring-2024-roster.json';
 
-// Get
+// Get It
+const h1 = document.querySelector('h1');
 const container = document.querySelector('.swiper-wrapper');
 
 // Fetch It
 fetch( dataSource )
   .then( response => response.json() )
   .then( students => { 
-
-    console.log(students);
      
     students.forEach( (student) => {
       
       // template
-      const template =`
-      <div class="swiper-slide">
-        
-         <img src="${student.Image}" />
-         <h1 style=" color: ${student.Color} ">${student.Emoji} 
-          
-         ${student.Name}
-         </h1>
-
-         <p>${student.Quote}</p>
-      
-
-        </div>
+      const template = `
+       <div class="swiper-slide">
+            
+            <h1 style='color: ${student.Color}'> ${student.Emoji}       ${student.Name}</h1>
+            <blockquote>${student.Quote}</blockquote>
+            <img style='border: ${student.Color} solid 8px' src=${student.Image}>
+            
+            <h2> ${student.Superpower}</h2>
+       </div>
       `
       
       //  dynamically insert      
@@ -37,4 +36,5 @@ fetch( dataSource )
       
     });
   
+        
 });
